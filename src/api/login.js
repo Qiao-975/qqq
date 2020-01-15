8a26d33b58c6198d4cfd41377b589f20492ade3f
Qiao <975155628@qq.com>
2020-01-15 21:44:12 +0800

    第一次

---
+++ src/api/login.js
@@ -0,0 +1,33 @@
+import request from '@/utils/request'
+
+export function login(username, password) {
+  return request({
+    url: '/user/login',
+    method: 'post',
+    data: {
+      username,
+      password
+    }
+  })
+}
+export function newlogin(data) {
+  return request({
+    url: '/web/Login/loginos',
+    method: 'post',
+    data: data
+  })
+}
+export function getInfo(token) {
+  return request({
+    url: '/user/info',
+    method: 'get',
+    params: { token }
+  })
+}
+
+export function logout() {
+  return request({
+    url: '/user/logout',
+    method: 'post'
+  })
+}
